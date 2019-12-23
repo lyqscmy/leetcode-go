cb4b796cb23a41c21a1d3636e9d08e9255c2da44
joestar <joestar@lwork.com>
2019-12-23 19:46:11 +0800

    htod and dtoh

--- leetcode.go
+++ leetcode.go
@@ -29,3 +29,99 @@ func (root *TreeNode) findLeftMostBottomNodeImpl(index int, res []int) []int {
 	res = root.Right.findLeftMostBottomNodeImpl(index+1, res)
 	return res
 }
+
+type Deque struct {
+	items  []int
+	head   int
+	tail   int
+	length int
+}
+
+func NewDeque(n int) *Deque {
+	return &Deque{make([]int, n+1), 0, 0, 0}
+}
+
+func (d *Deque) AddFirst(e int) {
+	d.length++
+	d.head = dec(d.head, len(d.items))
+	d.items[d.head] = e
+}
+
+func dec(i int, modulus int) int {
+	if i == 0 {
+		i = modulus - 1
+	} else {
+		i--
+	}
+	return i
+}
+func (d *Deque) AddLast(e int) {}
+
+// func (d *Deque) RemoveFirst() int {}
+func (d *Deque) RemoveLast() int {
+	d.length--
+	d.tail = dec(d.tail, len(d.items))
+	return d.items[d.tail]
+}
+func (d *Deque) Len() int { return d.length }
+func (d *Deque) Items() []int {
+	items := make([]int, 0, d.length)
+	head := d.head
+	for head != d.tail {
+		items = append(items, d.items[head])
+		head = dec(head, len(d.items))
+	}
+	return items
+}
+
+// left to right = top to bottom
+func dtoh(d []int) []int {
+	n := len(d)
+	h := NewDeque(n)
+	for i := 0; i < n; i++ {
+		// step 2
+		if h.Len() != 0 {
+			h.AddFirst(h.RemoveLast())
+		}
+
+		// step 1
+		h.AddFirst(d[i])
+	}
+	return h.Items()
+}
+
+// left to right = top to bottom
+func htod(h []int) []int {
+	n := len(h)
+	d := make([]int, n)
+	for i := 0; i < n; i++ {
+		// step 1
+		d[i] = h[0]
+		h = h[1:]
+
+		// step 2
+		if len(h) != 0 {
+			x := h[0]
+			h = h[1:]
+			h = append(h, x)
+		}
+	}
+	return d
+}
+
+func Compare(a, b []int) bool {
+	if a == nil && b == nil {
+		return true
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
--- leetcode_test.go
+++ leetcode_test.go
@@ -12,3 +12,21 @@ func TestFindLeftMostBottomNode(t *testing.T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
+
+func TestHtod(t *testing.T) {
+	h := []int{1, 2, 3, 4}
+	want := []int{4, 2, 3, 1}
+	got := htod(h)
+	if Compare(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestDtoh(t *testing.T) {
+	want := []int{1, 2, 3, 4}
+	d := []int{4, 2, 3, 1}
+	got := dtoh(d)
+	if Compare(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
