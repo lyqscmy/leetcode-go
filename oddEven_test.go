ab4da1a8a801c6a59403452b0e36fa5ead8a3674
joestar <joestar@lwork.com>
2020-01-02 20:24:00 +0800

    oddEven

---
+++ oddEven_test.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import "testing"
+
+func Test_oddEven(t *testing.T) {
+	tests := []struct {
+		xs   []int
+		want []int
+	}{
+		{[]int{2, 1}, []int{1, 2}},
+		{[]int{3, 1, 4, 5, 2}, []int{1, 3, 5, 2, 4}},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			oddEven(tt.xs)
+			if !Compare(tt.xs, tt.want) {
+				t.Errorf("got %v, want %v", tt.xs, tt.want)
+			}
+		})
+	}
+}
