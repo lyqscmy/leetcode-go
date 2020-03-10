83c2f59a25e78d6822220e5228c54887904cbcc9
li Yongqiang <lyqscmy@icloud.com>
2020-03-10 18:43:14 +0800

    udpate

--- leetcode.go
+++ leetcode.go
@@ -1,18 +1,5 @@
 package leetcode
 
-/**
- * 1. a ^ a = 0 归零律
- * 2. a ^ 0 = a 恒等律
- * 3. a ^ b ^ c = a ^ c ^ b 交换律
- */
-func singleNumber(nums []int) int {
-	res := 0
-	for _, num := range nums {
-		res ^= num
-	}
-	return res
-}
-
 // // left to right = top to bottom
 // func dtoh(d []int) []int {
 // 	n := len(d)
